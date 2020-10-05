064287fd6a247acfefafa7ac47c9168de6307cf4
mrmoore6 <mrmoore@dmacc.edu>
2020-10-05 18:57:03 -0500

    hello

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,9 +1,10 @@
 def multiply_string(n, message):
     for i in range(0, n):
-        print(message)
+        message = message * n
+        return(message)
 
 if __name__ == '__main__':
-    multiply_string(4, "Mike")
+    print(multiply_string(4, "Mike"))
 
 
 
--- test_functions/test_string_functions.py
+++ test_functions/test_string_functions.py
@@ -3,7 +3,7 @@ from more_functions import string_functions
 
 class MyTestCase(unittest.TestCase):
     def test_multiple_string(self):
-        self.assertTrue("Mike,Mike,Mike,Mike", string_functions.multiply_string(4, "Mike"))
+        self.assertEquals("MikeMikeMikeMike", string_functions.multiply_string(4, "Mike"))
 
 
 
