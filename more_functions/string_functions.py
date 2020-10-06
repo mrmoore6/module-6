51eb5e56d5f4b1a594b382756a1cedbb4d7a0795
mrmoore6 <mrmoore@dmacc.edu>
2020-10-05 19:00:35 -0500

    hello

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,4 +1,15 @@
+"""
+Author: Michael Moore
+Date: 10/5/20
+
+This program takes a number and a string and returns the string times the number
+"""
 def multiply_string(n, message):
+    """
+    :param n: number parameter
+    :param message: string parameter
+    :return: returns message varible
+    """
     for i in range(0, n):
         message = message * n
         return(message)
